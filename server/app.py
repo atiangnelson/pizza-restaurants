f37c7158cbd59ad98c1d52dd64db8968ecdea7a2
Nelson <nelson.atiang@student.moringaschool.com>
2025-06-21 12:26:30 +0300

    setup Flask app with database and registared blueprints

--- server/app.py
+++ server/app.py
@@ -2,12 +2,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # or any other DB
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('server.config.Config')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from server.controllers.restaurant_controller import restaurant_bp
+    from server.controllers.pizza_controller import pizza_bp
+    from server.controllers.restaurant_pizza_controller import restaurant_pizza_bp
+
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restaurant_pizza_bp)
+
+    return app
 
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+if __name__ == '__main__':
+    app.run(debug=True)
 
